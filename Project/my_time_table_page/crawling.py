87ea1dfc08935e87d19fea6f45e4ff8480480246
yooooonshine <hl3470@naver.com>
2023-06-19 18:34:57 +0900

    Feat: 크롤링 주석 추가

--- Project/my_time_table_page/crawling.py
+++ Project/my_time_table_page/crawling.py
@@ -15,24 +15,24 @@ dropdown = Select(dr.find_element(By.XPATH, '//*[@id="select_abeek"]/tbody/tr[2]
 dropdown.select_by_value("20221")
 time.sleep(1)
 list = dr.find_element(By.XPATH, '//*[@id="table_seoul"]/tbody/tr[7]/td[3]')  # xpath로 element 가져오기
-act.click(list).perform()
+act.click(list).perform()  # 전공을 선택(클릭)
 time.sleep(1)
-dr.switch_to.window(dr.window_handles[1])
-dr.get_window_position(dr.window_handles[1])
+dr.switch_to.window(dr.window_handles[1])  # 페이지 변경
+dr.get_window_position(dr.window_handles[1])  # 페이지 변경
 
 class_elements = dr.find_element(By.ID, 'select_list')  # 동작 할 요소 선택
 class_element_body = class_elements.find_element(By.TAG_NAME, 'tbody')  # 동작 할 요소 선택
 elements_tr = class_element_body.find_elements(By.TAG_NAME, 'tr')  # 동작 할 요소 선택
 
-class_array = [] # 저장할 배열
+class_array = []  # 저장할 배열
 index = 0
-for element_tr in elements_tr:
-    temporary_array = []
-    elements_td = element_tr.find_elements(By.TAG_NAME, 'td')
-    for element_td in elements_td:
-        print(element_td.text)
-        temporary_array.append(element_td.text)
-    if len(temporary_array) >= 3: # 데이터 무결성 체크
+for element_tr in elements_tr:  # tr들을 for문으로 하나하나씩 가져온다
+    temporary_array = []  # 리스트 안에 원소, 즉 원소는 리스트
+    elements_td = element_tr.find_elements(By.TAG_NAME, 'td')  # tr안에 td요소들을 배열에 넣는다.
+    for element_td in elements_td:  # td 원소들을 하나씩 가져온다.
+        # print(element_td.text)
+        temporary_array.append(element_td.text)  # 원소들의 텍스트를 임시 배열(리스트 안 원소)에 넣는다.
+    if len(temporary_array) >= 3:  # 데이터 무결성 체크, 임시 배열이 강의라면 class 배열에 넣어준다.
         class_array.append(temporary_array)
 
 print(class_array)
