785e2d9ac7871fc34179a63189ad179d4af9b407
Leejw011201 <jiwoomiso@gmail.com>
2023-06-20 14:45:07 +0900

    feat: 시간표 알고리즘 수정

--- Project/my_time_table_page/timetable_algorithm.py
+++ Project/my_time_table_page/timetable_algorithm.py
@@ -5,8 +5,10 @@ group2 = [['알고리즘분석', '화5_수5_목5'], ['프로그래밍언어론',
 group3 = [['확인용', '월3_월4_월5'], ['확인용2', '화3_목4_목5'], ['확인용3', '화4_수3_수4']]
 
 all_groups = [group1, group2, group3]
+# 여기까진 정보 받아옴
 
-possible_combinations = []
+
+possible_combinations = []  # 가능한 모든 조합
 
 # 모든 그룹의 조합을 생성
 for combination in itertools.product(*all_groups):
@@ -33,20 +35,25 @@ for combination in itertools.product(*all_groups):
 # 가능한 조합을 시간 순서로 정렬
 sorted_combinations = sorted(possible_combinations, key=lambda x: (x[0][1:], x[1][1:]))
 
+possible_combinations_sort = []  # 가능한 모든 조합들의 요일별 배열
+week = ['월', '화', '수', '목', '금']  # 1주
 # 가능한 조합 출력
 print("가능한 조합:")
 for index, combination in enumerate(sorted_combinations, start=1):
     print(f"조합 {index}:")
-    day_schedule = {day: [] for day in ['월', '화', '수', '목', '금']}
+    day_schedule = {day: [''] * 12 for day in week}
 
     for course, time in combination:
         day = time[0]  # 시간표의 첫 번째 문자는 요일을 나타냄
-        period = time[1:]  # 시간표의 나머지 문자는 교시를 나타냄
-        day_schedule[day].append((course, period))
+        period = int(time[1:])  # 시간표의 나머지 문자는 교시를 나타냄
+        day_schedule[day][period - 1] = course
+        possible_combinations_sort.append(day_schedule)
 
-    for day, schedule in day_schedule.items():
-        if len(schedule) > 0:
-            print(f"- {day}:")
-            for course, period in schedule:
-                print(f"  - {course}: {period}")
+    # 디버깅
+    print("  월  화  수  목  금")
+    for period in range(12):
+        print(f"{period + 1:2d}", end="  ")
+        for day in week:
+            print(f"{day_schedule[day][period]:<4s}", end="  ")
+        print()
     print()
