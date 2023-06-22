ff530794007cef3acdee35aa71e5fc4f3820315c
Kim minseong <alstjd2627@naver.com>
2023-06-22 17:29:31 +0900

    fix: 시간 자르는 함수 고침, 그러나 두자리 교시는 아직 못고침

--- Project/my_time_table_page/testdb.py
+++ Project/my_time_table_page/testdb.py
@@ -1,36 +1,75 @@
 import pymysql
 import crawling
+
 conn = pymysql.connect(host='localhost', user='root',
                        password='hrimaly', db='test1', charset='utf8')
 
+# def cutting_time(time): #화5수7을 화5_수7로 언더바 경계로 자름
+#     if time == "":
+#         return
+#     num = {"0","1","2","3","4","5","6","7","8","9"}
+#     cut_time = []
+#     if time[0]:
+#         cut_time.append(time[0])
+#         cut_time.append(time[1])
+#     for i in range(2,len(time)):
+#         isNum = False
+#         for j in num:
+#             if time[i] == j:
+#                 isNum = True
+#                 break
+#         if isNum:
+#             cut_time.append(time[i])
+#         else:
+#             if time[i] ==",":
+#                 continue
+#             cut_time.append(",")
+#             cut_time.append(time[i])
+#     str =""
+#     for i in cut_time:
+#         str += i
+#
+#     return(str)
 
-def cutting_time(time): #화5수7을 화5_수7로 언더바 경계로 자름
+
+def cutting_time(time):
     if time == "":
         return
-    num = {"0","1","2","3","4","5","6","7","8","9"}
+    num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
     cut_time = []
     if time[0]:
         cut_time.append(time[0])
         cut_time.append(time[1])
-    for i in range(2,len(time)):
-        isNum = False
+    isNum = [False] * len(time)
+    for i in range(len(time)):
         for j in num:
             if time[i] == j:
-                isNum = True
+                isNum[i] = True
                 break
-        if isNum:
+    for i in range(2, len(time)):
+        if isNum[i] == False: # 숫자가 이닐경우
+            if time[i] == " ":
+                continue
+            elif time[i] == ",":
+                continue
+            else:
+                cut_time.append(',')
+                cut_time.append(time[i])
+        else:   # 숫자일 경우
+            now_index = i - 1
+            if isNum[now_index]:
+                while isNum[now_index]:
+                    now_index -= 1
+                cut_time.append(",")
+                cut_time.append(time[now_index])
             cut_time.append(time[i])
-        else:
-            #cut_time.append("_")
-            cut_time.append(time[i])
-    str =""
+    cut_time_str = ""
     for i in cut_time:
-        str += i
+        cut_time_str += i
+    return cut_time_str
 
-    return(str)
 
-
-def cutting_subject_name(name): #과목이름 중 영문 명 삭제
+def cutting_subject_name(name):  # 과목이름 중 영문 명 삭제
     new_name = ""
     for i in name:
         if i == "\n":
@@ -39,16 +78,20 @@ def cutting_subject_name(name): #과목이름 중 영문 명 삭제
     return new_name
 
 
-def insert(id, grade, department, name, professor, credit, time, classroom, note, completion): # 데이터베이스에 정보를 추가
+def insert(id, grade, department, name, professor, credit, time, classroom, note, completion):  # 데이터베이스에 정보를 추가
     sql = '''insert into subject 
-        values("{0}", "{1}", "{2}","{3}","{4}","{5}","{6}","{7}","{8}","{9}");'''.format(id, grade, department, cutting_subject_name(name), professor, credit, cutting_time(time), classroom, note, completion)
+        values("{0}", "{1}", "{2}","{3}","{4}","{5}","{6}","{7}","{8}","{9}");'''.format(id, grade, department,
+                                                                                         cutting_subject_name(name),
+                                                                                         professor, credit,
+                                                                                         cutting_time(time), classroom,
+                                                                                         note, completion)
     curs.execute(sql)
 
 
-def search(search_word, grade, credit, completion): # 검색어, 학년, 학점, 이수구분을 이용한 검색기능
+def search(search_word, grade, credit, completion):  # 검색어, 학년, 학점, 이수구분을 이용한 검색기능
     first = False
     sql = "select * from subject"
-    if search_word != "0": # 검색어가 전체가 아닐 때
+    if search_word != "0":  # 검색어가 전체가 아닐 때
         sql += " where sname like \"%" + search_word + "%\""
         first = True
     if grade != "0":
@@ -78,7 +121,6 @@ def search(search_word, grade, credit, completion): # 검색어, 학년, 학점,
             print(i)
 
 
-
 curs = conn.cursor()
 curs.execute("drop table if exists subject cascade")
 curs.execute(""" # 수업 테이블 생성
@@ -98,9 +140,7 @@ create table subject(
 # 크롤링한 데이터 DB에 넣음
 data = crawling.class_array
 for i in data:
-    insert(i[4],i[0],i[1],i[5],i[11],i[6],i[12],"classroom",i[13],i[3])
-
-
+    insert(i[4], i[0], i[1], i[5], i[11], i[6], i[12], "classroom", i[13], i[3])
 
 # DB 테스트 출력
 sql = ("select * from subject")
@@ -113,5 +153,3 @@ for cur_row in rows:
 
 conn.commit()
 conn.close()
-
-
