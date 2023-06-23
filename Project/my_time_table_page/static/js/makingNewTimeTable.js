d0e4716dcba897fac949023c4c9bd5ff0d91d158
yooooonshine <hl3470@naver.com>
2023-06-23 15:59:37 +0900

    Feat: 자동완성 함수 및 ajax 통신

--- Project/my_time_table_page/static/js/makingNewTimeTable.js
+++ Project/my_time_table_page/static/js/makingNewTimeTable.js
@@ -1,143 +1,431 @@
-
-        // 그룹의 번호를 재할당하는 함수
-        function sync_group_numbers() {
-            var groups = document.getElementsByClassName('original');
-            var remainingGroups = groups.length;
-
-            // 각 그룹에 순차적인 번호 할당
-            for (var i = 0; i < groups.length; i++) {
-                var group = groups[i];
-                var groupNumber = i + 1;
-                group.setAttribute('data-group', groupNumber);
-                group.querySelector('p').innerText = "그룹" + groupNumber;
-            }
-
-            return remainingGroups;
-        }
-
-        // 그룹을 추가하는 함수
-        function add_group() {
-            var field = document.getElementById('field');
-            var groupNumber = sync_group_numbers() + 1;        //그룹의 수 1증가
-
-              if (groupNumber > 10) { // 최대 10개까지 추가 가능
-                alert("그룹은 최대 10개까지 추가할 수 있습니다.");
-                return;
-            }
-
-            // 새로운 그룹내의 과목 추가
-            var div = document.createElement('div');
-            div.className = 'original';
-            div.setAttribute('data-group', groupNumber);
-            div.innerHTML = `
-                <div class="preset" style="display:none">
-                    <input type="text" name="test1" value="" style="width:100px">
-                    <input type="button" value="삭제" onclick="remove_item(this)">
-                </div>
-                <div class="groupRow">
-                    <p>그룹${groupNumber}</p>
-                    <input type="button" value="과목추가" onclick="add_item(this.parentNode.parentNode)">
-                    <input type="button" value="그룹 삭제" onclick="remove_group(this.parentNode.parentNode)">
-                </div>
-            `;
-            field.appendChild(div);
-        }
-
-
-        //과목 추가 함수
-        function add_item(parentNode) {
-            var div = document.createElement('div');
-            var preset = parentNode.querySelector('.preset');
-            div.innerHTML = preset.innerHTML;
-            parentNode.appendChild(div);
-        }
-
-        //그룹 삭제 함수
-        function remove_group(group) {
-            var remainingGroups = sync_group_numbers();
-
-             // 그룹의 수가 2개 이하인 경우 삭제 불가능함
-            if (remainingGroups <= 2) {
-                alert("시간표를 만들기 위해 2개 이상의 그룹이 필요합니다.");
-            } else {
-                var groupNumber = parseInt(group.getAttribute('data-group'));
-                group.parentNode.removeChild(group);
-
-                // 그룹 삭제 후 남은 그룹의 번호 재할당
-                sync_group_numbers();
+//csrf token 건들지 말것
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
         }
+    }
+    return cookieValue;
+}
 
-        //과목 삭제 함수
-        function remove_item(obj) {
-            obj.parentNode.parentNode.removeChild(obj.parentNode);
-        }
-    // 시간표 데이터
-    var timetable = [
-      { major: "대예이", time: "월23", year: 3, credit: 3 },
-      { major: "컴프", time: "화13", year: 2, credit: 2 },
-      { major: "대학물리", time: "월10", year: 4, credit: 4 },
-      { major: "영어", time: "화09", year: 3, credit: 1 },
-    ];
+var csrftoken = getCookie('csrftoken');
 
-    function sortTimetable(sortKey) {
-      timetable.sort(function(a, b) {
-        if (sortKey === "time") {
-          // 시간에 대한 정렬
-          var dayA = a.time.substr(0, 2);
-          var dayB = b.time.substr(0, 2);
-          var hourA = parseInt(a.time.substr(2));
-          var hourB = parseInt(b.time.substr(2));
 
-          if (dayA !== dayB) {
-            // 요일이 다른 경우
-            var days = ["월", "화", "수", "목", "금"];
-            return days.indexOf(dayA) - days.indexOf(dayB);
-          } else {
-            // 요일이 같은 경우
-            return hourA - hourB;
-          }
-        } else {
-          // 다른 키에 대한 정렬
-          return a[sortKey] > b[sortKey] ? 1 : -1;
-        }
-      });
 
-      displayTimetable();
+
+// 그룹의 번호를 재할당하는 함수
+function sync_group_numbers() {
+    var groups = document.getElementsByClassName('original');
+    var remainingGroups = groups.length;
+
+    // 각 그룹에 순차적인 번호 할당
+    for (var i = 0; i < groups.length; i++) {
+        var group = groups[i];
+        var groupNumber = i + 1;
+        group.setAttribute('data-group', groupNumber);
+        group.querySelector('p').innerText = "그룹" + groupNumber;
     }
 
-    function displayTimetable() {
-      var table = document.getElementById("timetable");
+    return remainingGroups;
+}
 
-      // 기존 테이블 내용 삭제
-      while (table.firstChild) {
-        table.removeChild(table.firstChild);
+// 그룹을 추가하는 함수
+function add_group() {
+    var field = document.getElementById('field');
+    var groupNumber = sync_group_numbers() + 1;        //그룹의 수 1증가
+
+      if (groupNumber > 10) { // 최대 10개까지 추가 가능
+        alert("그룹은 최대 10개까지 추가할 수 있습니다.");
+        return;
+    }
+
+    // 새로운 그룹내의 과목 추가
+    var div = document.createElement('div');
+    div.className = 'original';
+    div.setAttribute('data-group', groupNumber);
+    div.innerHTML = `
+        <div class="preset" style="display:none">
+            <input type="text" name="test1" value="" style="width:100px">
+            <input type="button" value="삭제" onclick="remove_item(this)">
+        </div>
+        <div class="groupRow">
+            <p>그룹${groupNumber}</p>
+            <input type="button" value="과목추가" onclick="add_item(this.parentNode.parentNode)">
+            <input type="button" value="그룹 삭제" onclick="remove_group(this.parentNode.parentNode)">
+        </div>
+    `;
+    field.appendChild(div);
+}
+
+
+//과목 추가 함수
+function add_item(parentNode) {
+    var div = document.createElement('div');
+    var preset = parentNode.querySelector('.preset');
+    div.innerHTML = preset.innerHTML;
+    parentNode.appendChild(div);
+}
+
+//그룹 삭제 함수
+function remove_group(group) {
+    var remainingGroups = sync_group_numbers();
+
+     // 그룹의 수가 2개 이하인 경우 삭제 불가능함
+    if (remainingGroups <= 2) {
+        alert("시간표를 만들기 위해 2개 이상의 그룹이 필요합니다.");
+    } else {
+        var groupNumber = parseInt(group.getAttribute('data-group'));
+        group.parentNode.removeChild(group);
+
+        // 그룹 삭제 후 남은 그룹의 번호 재할당
+        sync_group_numbers();
+    }
+}
+
+//과목 삭제 함수
+function remove_item(obj) {
+    obj.parentNode.parentNode.removeChild(obj.parentNode);
+}
+// 시간표 데이터
+var timetable = [
+  { major: "대예이", time: "월23", year: 3, credit: 3 },
+  { major: "컴프", time: "화13", year: 2, credit: 2 },
+  { major: "대학물리", time: "월10", year: 4, credit: 4 },
+  { major: "영어", time: "화09", year: 3, credit: 1 },
+];
+
+function sortTimetable(sortKey) {
+  timetable.sort(function(a, b) {
+    if (sortKey === "time") {
+      // 시간에 대한 정렬
+      var dayA = a.time.substr(0, 2);
+      var dayB = b.time.substr(0, 2);
+      var hourA = parseInt(a.time.substr(2));
+      var hourB = parseInt(b.time.substr(2));
+
+      if (dayA !== dayB) {
+        // 요일이 다른 경우
+        var days = ["월", "화", "수", "목", "금"];
+        return days.indexOf(dayA) - days.indexOf(dayB);
+      } else {
+        // 요일이 같은 경우
+        return hourA - hourB;
       }
-
-      // 정렬된 시간표 출력
-      timetable.forEach(function(course) {
-        var row = document.createElement("tr");
-
-        // 과목
-        var majorCell = document.createElement("td");
-        majorCell.textContent = course.major;
-        row.appendChild(majorCell);
-
-        // 시간
-        var timeCell = document.createElement("td");
-        timeCell.textContent = course.time;
-        row.appendChild(timeCell);
-
-        // 학년
-        var yearCell = document.createElement("td");
-        yearCell.textContent = course.year;
-        row.appendChild(yearCell);
-
-        // 학점
-        var creditCell = document.createElement("td");
-        creditCell.textContent = course.credit;
-        row.appendChild(creditCell);
-
-        table.appendChild(row);
-      });
+    } else {
+      // 다른 키에 대한 정렬
+      return a[sortKey] > b[sortKey] ? 1 : -1;
     }
+  });
+
+  displayTimetable();
+}
+
+function displayTimetable() {
+  var table = document.getElementById("timetable");
+
+  // 기존 테이블 내용 삭제
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
+
+  // 정렬된 시간표 출력
+  timetable.forEach(function(course) {
+    var row = document.createElement("tr");
+
+    // 과목
+    var majorCell = document.createElement("td");
+    majorCell.textContent = course.major;
+    row.appendChild(majorCell);
+
+    // 시간
+    var timeCell = document.createElement("td");
+    timeCell.textContent = course.time;
+    row.appendChild(timeCell);
+
+    // 학년
+    var yearCell = document.createElement("td");
+    yearCell.textContent = course.year;
+    row.appendChild(yearCell);
+
+    // 학점
+    var creditCell = document.createElement("td");
+    creditCell.textContent = course.credit;
+    row.appendChild(creditCell);
+
+    table.appendChild(row);
+  });
+}
+
+// resultBox에 요소 넣기
+function pushClassData(textList)
+{
+    a = document.createElement("DIV");
+    a.innerHTML = textList[0][2]
+    document.getElementById("resultBox").appendChild(a)
+}
+
+
+function sendingData(inp) { //inp는 input객체
+
+    if (inp == document.getElementById('autoInput')) {
+        boolDepartureCheck = false;
+    } else {
+        boolDestinationCheck = false;
+    }
+    $.ajax({
+        url: 'recommend',
+        type: 'POST',
+        data: {
+            'input_val': inp.value.toUpperCase(), //대문자 변환해서 소문자도 검색가능
+            'csrfmiddlewaretoken': csrftoken,
+        },
+        datatype: 'json',
+        success: function (data) {
+            receivedList = data['recommendations'];
+            autocomplete.setAutocomplete(inp, receivedList); //autocomplete함수를 input객체를 받아 실행
+            autocomplete.inputEvent(nowKeyboardCode);
+            autocomplete.keydownEvent(nowKeyboardCode);
+            console.log(receivedList);
+        }
+    });
+}
+textList = [['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-1', '미술의이해\n(COMPREHENSION OF ART)', 'C506', '3', '10/20/30/40/50', '55', '20', '비공학', '전영백', '화789', '미술대 수강불가/강의요원 여서영 ', '공통교양\n(서울)', '예술과디자인'], ['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-2', '미술의이해\n(COMPREHENSION OF ART)', 'C807', '3', '10/20/30/40/50', '47', '20', '공학', '손수연', '화789', '미술대 수강불가 ', '공통교양\n(서울)', '예술과디자인']]
+
+function submitCheck(event) {
+    //submit될 때 페이지 리로드 방지
+    //event.preventDefault();
+    var timeTableQuery = document.getElementById("autoInput").value;
+
+    //출발지 도착지형식이 참이면 백으로 출발지 도착지 보내기
+
+        //결과경로창 보이게끔
+
+    $.ajax({
+        url: 'query_submit',
+        type: 'POST',
+        data: {
+            'time_table_query': timeTableQuery,
+            'csrfmiddlewaretoken': csrftoken,
+        },
+        datatype: 'json',
+        beforeSend: function (request) {
+            // Performed before calling Ajax
+            $("#mySpinner").show();
+
+        },
+        success: function (data) {
+
+            $("#mySpinner").hide();
+            // textList = data;
+            pushClassData(textList)
+        },
+
+    });
+
+}
+
+
+
+//##자동완성
+// autocomplete 부분을 생성
+let autocomplete = (function () {
+
+    let _inp = null;
+
+    let _arr = [];
+
+    let _currentFocus;
+
+    let _setAutocomplete = function (inp, arr) {
+        // autocomplete할 배열
+        _arr = arr;
+
+
+        // 기존의 input 값과 같지 않다면, 리스너 해제
+        if (_inp === inp) {
+            return;
+        }
+
+        // 기존 리스너해제
+        _removeListener();
+
+        // 새로운 input 의 리스너 추가.
+        _inp = inp;
+    }
+
+    let inputEvent = function (keyCord) {
+        //화살표 및 엔터면 출력된 자동완성 초기화 안한다
+        if (keyCord == 40 || keyCord == 38 || keyCord == 13) {
+            return false;
+        }
+        var a, b, i, val = _inp.value;//a,b,i는 지정되지않고 val만 입력값으로 저장
+        // 이전 생성된 div 제거
+        closeAllLists();
+
+        // 요소 확인
+        if (!val) {
+            return false;
+        }
+
+        // 현재의 포커스의 위치는 없음.
+        _currentFocus = -1;
+
+        // autocomplet에서 항목을 보여줄 div 생성하고 이를 a에 준다.
+        a = document.createElement("DIV");
+        //
+        a.setAttribute("id", _inp.id + "autocomplete-list");//속성주기
+        // css 적용
+        a.setAttribute("class", "autocomplete-items");
+
+        // input 아래의 div 붙이기.
+        _inp.parentNode.appendChild(a);
+
+        // autocomplet할 요소 찾기
+        for (i = 0; i < _arr.length; i++) {
+            // 배열의 요소를 현재 input의 value의 값만큼 자른 후, 같으면 추가한다.
+
+            b = document.createElement("DIV");
+            // value의 값 만큼 굵게 표시
+            b.innerHTML = "<strong>" + _arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += _arr[i].substr(val.length);
+            b.innerHTML += "<input type='hidden' value='" + _arr[i] + "'>";
+
+            // 생성된 div에서 이벤트 발생시 hidden으로 생성된 input안의 value의 값을 autocomplete할 요소에 넣기
+            b.addEventListener("click", function (e) {
+                if (_inp == document.getElementById('autoInput')) {
+                    boolDepartureCheck = true;
+                } else {
+                    boolDestinationCheck = true;
+                }
+                _inp.value = this.getElementsByTagName("input")[0].value;
+                closeAllLists();
+            });
+
+            // autocomplete 리스트를 붙이기.
+            a.appendChild(b);
+
+        }
+    }
+    let lastkeyCord = 40;
+
+    let keydownEvent = function (keyCord) {
+        var x = document.getElementById(_inp.id + "autocomplete-list");
+        // 선택할 요소 없으면 null ,
+        if (x) {
+            // 태그 네임을 가지는 엘리먼트의 유요한 html 컬렉션을 반환.
+            // div의 값을 htmlCollection의 값으로 받아옴.
+            x = x.getElementsByTagName("div");
+        }
+
+        if (keyCord == 40) {
+            // down
+            // 현재위치 증가
+            _currentFocus++;
+            // 현재위치의 포커스 나타내기
+            addActive(x, keyCord, lastkeyCord);
+        } else if (keyCord == 38) {
+            // up
+            // 현재위치 감소
+            _currentFocus--;
+            // 현재위치의 포커스 나타내기
+            addActive(x, keyCord, lastkeyCord);
+        } else if (keyCord == 13) {
+            // enter
+            if (_inp == document.getElementById('autoInput')) {
+                for (let list in $('#autoInput1autocomplete-list div')) {
+                    if (list.html == _inp.innerHTML){
+                        boolDepartureCheck = true;
+                    }
+                }
+
+            } else {
+                for (let list in $('#autoInputautocomplete-list div')) {
+                    if (list.html == _inp.innerHTML){
+                        boolDestinationCheck = true;
+                    }
+                }
+
+            }
+            // keyCord.preventDefault();
+            // 현재위치가 아이템 선택창내에 있는 경우
+            if (_currentFocus > -1) {
+                // 현재 위치의 값 클릭
+                if (x) x[_currentFocus].click();
+            }
+        }
+        lastkeyCord = keyCord;
+    }
+    //바깥 클릭하면 자동완성 사라짐
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+
+
+    let addActive = function (x, keyCode, lastkeyCord) {
+        let now_keyCode = keyCode;
+        let last_keyCode = lastkeyCord
+        if (!x) return false;
+        removeActive(x);
+        if (_currentFocus >= x.length) _currentFocus = 0;
+        if (_currentFocus < 0) _currentFocus = (x.length - 1);
+        x[_currentFocus].classList.add("autocomplete-active");
+        // 키다운이벤트 따라가기
+
+        if (last_keyCode != now_keyCode && _currentFocus % 6 == 5) {
+            $('.autocomplete-items').scrollTop((_currentFocus - 5) * 28);
+        }
+
+        if (_currentFocus % 6 == 0 && now_keyCode == 40) {
+            $('.autocomplete-items').scrollTop(_currentFocus * 28);
+        } else if (_currentFocus % 6 == 0 && now_keyCode == 38) {
+            $('.autocomplete-items').scrollTop((_currentFocus - 5) * 28);
+        }
+
+    }
+
+
+    let removeActive = function (x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+
+    let closeAllLists = function (elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != _inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+
+    let _removeListener = function () {
+        if (_inp !== null) {
+            console.log(_inp)
+            _inp.removeEventListener("keyup", inputEvent, false);
+            _inp.removeEventListener("keydown", keydownEvent, false);
+        }
+    }
+    return {
+
+        setAutocomplete: function (inp, arr) {
+            _setAutocomplete(inp, arr);
+        },
+        inputEvent: function (keyCode) {
+            inputEvent(keyCode);
+        },
+        keydownEvent: function (keyCode) {
+            keydownEvent(keyCode);
+        }
+    }
+
+})();
