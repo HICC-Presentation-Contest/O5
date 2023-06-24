693f7eb07fb9d2d3a9cfbf0d254c9dd59ca7738e
yooooonshine <hl3470@naver.com>
2023-06-24 16:52:12 +0900

    Feat: 모든 페이지에 기본 틀 적용 Design: 주 색상 적용

--- Project/my_time_table_page/static/js/makingNewTimeTable.js
+++ Project/my_time_table_page/static/js/makingNewTimeTable.js
@@ -366,6 +366,7 @@ let autocomplete = (function () {
 
 })();
 
+let receivedList = [];
 
 function suggestedSearchWord(inp) { //inp는 input객체
     $.ajax({
@@ -377,13 +378,34 @@ function suggestedSearchWord(inp) { //inp는 input객체
         },
         datatype: 'json',
         success: function (data) {
-            receivedList = data['recommendations'];
+            receivedList = data['suggested_search_word_list'];
             autocomplete.setAutocomplete(inp, receivedList); //autocomplete함수를 input객체를 받아 실행
             autocomplete.inputEvent(nowKeyboardCode);
             autocomplete.keydownEvent(nowKeyboardCode);
             console.log(receivedList);
         }
     });
+}
+
+
+// resultBox에 요소 넣기
+var textList = [['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-1', '미술의이해\n(COMPREHENSION OF ART)', 'C506', '3', '10/20/30/40/50', '55', '20', '비공학', '전영백', '화789', '미술대 수강불가/강의요원 여서영 ', '공통교양\n(서울)', '예술과디자인'],
+    ['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-2', '미술의이해\n(COMPREHENSION OF ART)', 'C807', '3', '10/20/30/40/50', '47', '20', '공학', '손수연', '화789', '미술대 수강불가 ', '공통교양\n(서울)', '예술과디자인']]
+
+function pushClassData(textData) {
+    var a = document.createElement("div");
+    a.innerHTML = '<div class="result">' +
+        '<div class="subject">' + textData[6] + '</div>' +
+        '<div class="nameTime">' + textData[13] + ' ' + textData[14] + '</div> ' +
+        '<div class="detail">' + textData[0] + '학년 ' + textData[3] + ' ' + textData[8] + '학점 ' + textData[5] + '</div>' +
+        '</div>';
+    document.getElementById("resultBox").appendChild(a);
+}
+
+function displayTextList() {
+    for (var i = 0; i < textList.length; i++) {
+        pushClassData(textList[i]);
+    }
 }
 
 
@@ -415,7 +437,7 @@ function searchWordSubmit() {
 
             $("#mySpinner").hide();
             // textList = data;
-            pushClassData(textList)
+            displayTextList()
         },
 
     });
@@ -423,22 +445,3 @@ function searchWordSubmit() {
 }
 
 
-// resultBox에 요소 넣기
-var textList = [['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-1', '미술의이해\n(COMPREHENSION OF ART)', 'C506', '3', '10/20/30/40/50', '55', '20', '비공학', '전영백', '화789', '미술대 수강불가/강의요원 여서영 ', '공통교양\n(서울)', '예술과디자인'],
-    ['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-2', '미술의이해\n(COMPREHENSION OF ART)', 'C807', '3', '10/20/30/40/50', '47', '20', '공학', '손수연', '화789', '미술대 수강불가 ', '공통교양\n(서울)', '예술과디자인']]
-
-function pushClassData(textData) {
-    var a = document.createElement("div");
-    a.innerHTML = '<div class="result">' +
-        '<div class="subject">' + textData[6] + '</div>' +
-        '<div class="nameTime">' + textData[13] + ' ' + textData[14] + '</div> ' +
-        '<div class="detail">' + textData[0] + '학년 ' + textData[3] + ' ' + textData[8] + '학점 ' + textData[5] + '</div>' +
-        '</div>';
-    document.getElementById("resultBox").appendChild(a);
-}
-
-function displayTextList() {
-    for (var i = 0; i < textList.length; i++) {
-        pushClassData(textList[i]);
-    }
-}
