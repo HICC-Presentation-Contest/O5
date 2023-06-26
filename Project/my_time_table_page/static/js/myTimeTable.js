e2db9e58aaa8ae25e505f6cabb90375495d39c9b
yooooonshine <hl3470@naver.com>
2023-06-26 19:35:46 +0900

    Feat: myTimeTable페이지 왼쪽페이지 구현 Fix: makingNewTimeTable 코드 오타 수정

--- Project/my_time_table_page/static/js/myTimeTable.js
+++ Project/my_time_table_page/static/js/myTimeTable.js
@@ -1,122 +1,228 @@
 
-
 // 크롤링 에시
 var TimeTableClass =
-	{
-		'기본시간표1':[
-			['스페인어', '월234'],
-			['컴퓨터구조', '화2_수2_목2'],
-			['컴퓨터네트워크', '수3_금23'],
-			['알고리즘분석', '화5_수5_목5'],
-			['프로그래밍언어론', '화9_금56']
-		],
-		'기본시간표2':[
-			['스페인어', '토234'],
-			['컴퓨터구조', '화2_수2_목2'],
-			['컴퓨터네트워크', '수3_금23'],
-			['알고리즘분석', '화5_수5_목5'],
-			['프로그래밍언어론', '화9_금56']
-		],
-		'기본시간표3':[
-			['스페인어', '일234'],
-			['컴퓨터구조', '화2_수2_목2'],
-			['컴퓨터네트워크', '수3_금23'],
-			['알고리즘분석', '화5_수5_목5'],
-			['프로그래밍언어론', '화9_금56']
-		],
+    {
+        '기본시간표1': [
+            ['스페인어', '월234'],
+            ['컴퓨터구조', '화2_수2_목2'],
+            ['컴퓨터네트워크', '수3_금23'],
+            ['알고리즘분석', '화5_수5_목5'],
+            ['프로그래밍언어론', '화9_금56']
+        ],
+        '기본시간표2': [
+            ['스페인어', '토234'],
+            ['컴퓨터구조', '화2_수2_목2'],
+            ['컴퓨터네트워크', '수3_금23'],
+            ['알고리즘분석', '화5_수5_목5'],
+            ['프로그래밍언어론', '화9_금56']
+        ],
+        '기본시간표3': [
+            ['스페인어', '일234'],
+            ['컴퓨터구조', '화2_수2_목2'],
+            ['컴퓨터네트워크', '수3_금23'],
+            ['알고리즘분석', '화5_수5_목5'],
+            ['프로그래밍언어론', '화9_금56']
+        ],
 
-	}
+    }
 ;
 
 // 요일 한국어에서 영어로 변환
-function KorToEngOfDay(day)
-{
-	switch (day) {
-	case '월':
-		return 'mon';
-	case '화':
-		return 'tue';
-	case '수':
-		return 'wed';
-	case '목':
-		return 'thu';
-	case '금':
-		return 'fri';
-	case '토':
-		return 'sat';
-	case '일':
-		return 'sun';
-	default:
-		return 0;
-	}
+function KorToEngOfDay(day) {
+    switch (day) {
+        case '월':
+            return 'mon';
+        case '화':
+            return 'tue';
+        case '수':
+            return 'wed';
+        case '목':
+            return 'thu';
+        case '금':
+            return 'fri';
+        case '토':
+            return 'sat';
+        case '일':
+            return 'sun';
+        default:
+            return 0;
+    }
 }
 
-function addClassToTimeTable(TimeTableClass)
-{
-	// 수업이름
-	let className = '';
-	// 시간 정보
-	let timeList = '';
-	// 수업의 요일
-	let day = '';
-	// 수업의 요일의 시간
-	let timeOfDay = '';
-	// html에 넣을 id
-	let id = '';
-	// timeTableClass 각 수업마다, 이름, 시간으로 쪼갠다.
-	for(let i=0; i < TimeTableClass.length ;i++)
-	{
-		className = TimeTableClass[i][0];
-		timeList = TimeTableClass[i][1].split("_"); // 월2 화2 수2 이렇게 쪼개진다.
-		for (let j = 0; j < timeList.length; j++) // 월234는 한번.
-		{
-			day = KorToEngOfDay(timeList[j].substr(0,1)); // 월234에서 월 가져온다.
-			timeOfDay = timeList[j].substring(1); // 2,3,4 가져온다.
-			for (let k = 0; k < timeOfDay.length; k++)
-			{
-				id = "#timeTable_" + day + "_" + timeOfDay.substr(k,1);
-				$(id).text(className);
-			}
+function timeTableRemover(){
+    document.getElementById('myTimeTable').innerHTML =
+        "<tr>\n" +
+        "                <td scope=\"col\" ;></td>\n" +
+        "                <td scope=\"col\" ;>월</td>\n" +
+        "                <td scope=\"col\" ;>화</td>\n" +
+        "                <td scope=\"col\" ;>수</td>\n" +
+        "                <td scope=\"col\" ;>목</td>\n" +
+        "                <td scope=\"col\" ;>금</td>\n" +
+        "                <td scope=\"col\" ;>토</td>\n" +
+        "                <td scope=\"col\" ;>일</td>\n" +
+        "            </tr>\n" +
+        "            <!-- 1교시 -->\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">9:00 - 10:00</td>\n" +
+        "                <td id=\"timeTable_mon_1\"></td>\n" +
+        "                <td id=\"timeTable_tue_1\"></td>\n" +
+        "                <td id=\"timeTable_wed_1\"></td>\n" +
+        "                <td id=\"timeTable_thu_1\"></td>\n" +
+        "                <td id=\"timeTable_fri_1\"></td>\n" +
+        "                <td id=\"timeTable_sat_1\"></td>\n" +
+        "                <td id=\"timeTable_sun_1\"></td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">10:00 - 11:00</td>\n" +
+        "                <td id=\"timeTable_mon_2\"></td>\n" +
+        "                <td id=\"timeTable_tue_2\"></td>\n" +
+        "                <td id=\"timeTable_wed_2\"></td>\n" +
+        "                <td id=\"timeTable_thu_2\"></td>\n" +
+        "                <td id=\"timeTable_fri_2\"></td>\n" +
+        "                <td id=\"timeTable_sat_2\"></td>\n" +
+        "                <td id=\"timeTable_sun_2\"></td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">11:00 - 12:00</td>\n" +
+        "                <td id=\"timeTable_mon_3\"></td>\n" +
+        "                <td id=\"timeTable_tue_3\"></td>\n" +
+        "                <td id=\"timeTable_wed_3\"></td>\n" +
+        "                <td id=\"timeTable_thu_3\"></td>\n" +
+        "                <td id=\"timeTable_fri_3\"></td>\n" +
+        "                <td id=\"timeTable_sat_3\"></td>\n" +
+        "                <td id=\"timeTable_sun_3\"></td>\n" +
+        "            </tr>\n" +
+        "\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">12:00 - 13:00</td>\n" +
+        "                <td id=\"timeTable_mon_4\"></td>\n" +
+        "                <td id=\"timeTable_tue_4\"></td>\n" +
+        "                <td id=\"timeTable_wed_4\"></td>\n" +
+        "                <td id=\"timeTable_thu_4\"></td>\n" +
+        "                <td id=\"timeTable_fri_4\"></td>\n" +
+        "                <td id=\"timeTable_sat_4\"></td>\n" +
+        "                <td id=\"timeTable_sun_4\"></td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">13:00 - 14:00</td>\n" +
+        "                <td id=\"timeTable_mon_5\"></td>\n" +
+        "                <td id=\"timeTable_tue_5\"></td>\n" +
+        "                <td id=\"timeTable_wed_5\"></td>\n" +
+        "                <td id=\"timeTable_thu_5\"></td>\n" +
+        "                <td id=\"timeTable_fri_5\"></td>\n" +
+        "                <td id=\"timeTable_sat_5\"></td>\n" +
+        "                <td id=\"timeTable_sun_5\"></td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">14:00 - 15:00</td>\n" +
+        "                <td id=\"timeTable_mon_6\"></td>\n" +
+        "                <td id=\"timeTable_tue_6\"></td>\n" +
+        "                <td id=\"timeTable_wed_6\"></td>\n" +
+        "                <td id=\"timeTable_thu_6\"></td>\n" +
+        "                <td id=\"timeTable_fri_6\"></td>\n" +
+        "                <td id=\"timeTable_sat_6\"></td>\n" +
+        "                <td id=\"timeTable_sun_6\"></td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">15:00 - 16:00</td>\n" +
+        "                <td id=\"timeTable_mon_7\"></td>\n" +
+        "                <td id=\"timeTable_tue_7\"></td>\n" +
+        "                <td id=\"timeTable_wed_7\"></td>\n" +
+        "                <td id=\"timeTable_thu_7\"></td>\n" +
+        "                <td id=\"timeTable_fri_7\"></td>\n" +
+        "                <td id=\"timeTable_sat_7\"></td>\n" +
+        "                <td id=\"timeTable_sun_7\"></td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">16:00 - 17:00</td>\n" +
+        "                <td id=\"timeTable_mon_8\"></td>\n" +
+        "                <td id=\"timeTable_tue_8\"></td>\n" +
+        "                <td id=\"timeTable_wed_8\"></td>\n" +
+        "                <td id=\"timeTable_thu_8\"></td>\n" +
+        "                <td id=\"timeTable_fri_8\"></td>\n" +
+        "                <td id=\"timeTable_sat_8\"></td>\n" +
+        "                <td id=\"timeTable_sun_8\"></td>\n" +
+        "            </tr>\n" +
+        "            <tr>\n" +
+        "                <td class=\"myTimeTable_time display-5\">17:00 - 18:00</td>\n" +
+        "                <td id=\"timeTable_mon_9\"></td>\n" +
+        "                <td id=\"timeTable_tue_9\"></td>\n" +
+        "                <td id=\"timeTable_wed_9\"></td>\n" +
+        "                <td id=\"timeTable_thu_9\"></td>\n" +
+        "                <td id=\"timeTable_fri_9\"></td>\n" +
+        "                <td id=\"timeTable_sat_9\"></td>\n" +
+        "                <td id=\"timeTable_sun_9\"></td>\n" +
+        "            </tr>"
+}
 
-		}
+function addClassToTimeTable(TimeTableClass) {
+    timeTableRemover()
+    // 수업이름
+    let className = '';
+    // 시간 정보
+    let timeList = '';
+    // 수업의 요일
+    let day = '';
+    // 수업의 요일의 시간
+    let timeOfDay = '';
+    // html에 넣을 id
+    let id = '';
+    // timeTableClass 각 수업마다, 이름, 시간으로 쪼갠다.
+    for (let i = 0; i < TimeTableClass.length; i++) {
+        className = TimeTableClass[i][0];
+        timeList = TimeTableClass[i][1].split("_"); // 월2 화2 수2 이렇게 쪼개진다.
+        for (let j = 0; j < timeList.length; j++) // 월234는 한번.
+        {
+            day = KorToEngOfDay(timeList[j].substr(0, 1)); // 월234에서 월 가져온다.
+            timeOfDay = timeList[j].substring(1); // 2,3,4 가져온다.
+            for (let k = 0; k < timeOfDay.length; k++) {
+                id = "#timeTable_" + day + "_" + timeOfDay.substr(k, 1);
+                $(id).text(className);
+            }
 
-	}
+        }
+
+    }
 
 }
 
 
-// 딕셔너리를 받아서 개인 시간표 세팅하기 안의 함수
-function basicUserInformation(basicTimeTableName)
-{
-
-	$('#myTimeTableName').html(basicTimeTableName);
-	innerList = document.createElement('button');
-	$(innerList).addClass( "list-group-item list-group-item-action" );
-	$(innerList).text(basicTimeTableName);
-	$('#timeTableNameList').append(innerList);
-}
+// // 딕셔너리를 받아서 개인 시간표 세팅하기 안의 함수
+// function basicUserInformation(basicTimeTableName)
+// {
+//
+// 	$('#myTimeTableName').html(basicTimeTableName);
+// 	innerList = document.createElement('button');
+// 	$(innerList).addClass( "list-group-item list-group-item-action" );
+// 	$(innerList).text(basicTimeTableName);
+// 	$('#timeTableNameList').append(innerList);
+// }
 
 
 // 딕셔너리를 받아서 개인 시간표 세팅하기
-function basicUserInformation(TimeTableClass)
-{
-	$('#timeTableNameList').empty()
-	let timeTableNameList = Object.keys(TimeTableClass)   // 리스트의 키값은 시간표이름
-	for(let i=0; i < timeTableNameList.length; i++) {
-		$('#myTimeTableName').html(timeTableNameList[i]);
-		innerList = document.createElement('button');
-		$(innerList).addClass( "list-group-item list-group-item-action" );
-		$(innerList).text(timeTableNameList[i]);
-		$('#timeTableNameList').append(innerList);
-	}
-	addClassToTimeTable(TimeTableClass[timeTableNameList[0]]);
+function basicUserInformation(TimeTableClass) {
+    $('#timeTableNameList').empty(); // 기존 리스트 지우기
+    let timeTableNameList = Object.keys(TimeTableClass)   // 리스트의 키값은 시간표이름
+    for (let i = 0; i < timeTableNameList.length; i++) { //키값에 맞게 타임테이블이름 리스트에 추가해준다.
+        $('#myTimeTableName').html(timeTableNameList[i]);
+        let innerList = document.createElement('button');
+        $(innerList).addClass("list-group-item list-group-item-action");
+        $(innerList).text(timeTableNameList[i]);
+        // $(innerList).click(addClassToTimeTable(this.text()))
+        $(innerList).attr('onclick', 'timeTableNameClick(this)')
+        $('#timeTableNameList').append(innerList);
+    }
+    addClassToTimeTable(TimeTableClass[timeTableNameList[0]]);
 
 }
 
+function timeTableNameClick(event){
+    let myTimeTable = TimeTableClass[event.innerHTML];
+    addClassToTimeTable(myTimeTable)
+}
 
-
-window.onload=function(){
-	basicUserInformation(TimeTableClass);
+window.onload = function () {
+    basicUserInformation(TimeTableClass);
 
 }
 
