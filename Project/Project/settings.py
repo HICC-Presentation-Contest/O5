77ce02ffca7472ba6bf428e64f345978d62df930
Kim minseong <alstjd2627@naver.com>
2023-06-26 12:21:18 +0900

    feat: views.py에 검색어 기능 추가 테스트

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test1',
         'USER': 'root',
-        'PASSWORD': 'dhqlfkrj1@',
+        'PASSWORD': 'hrimaly',
         'HOST': 'localhost',
         'PORT': '3306',
     }
--- Project/my_time_table_page/testdb.py
+++ Project/my_time_table_page/testdb.py
@@ -1,6 +1,6 @@
 import pymysql
 # import all_list_crawling
-import test_read
+# import test_read
 
 conn = pymysql.connect(host='localhost', user='root',
                        password='hrimaly', db='test1', charset='utf8')
@@ -37,7 +37,7 @@ conn = pymysql.connect(host='localhost', user='root',
 def cutting_time(time):
     if time == "":
         return
-    num = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+    num = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10","11","12"}
     cut_time = []
     if time[0]:
         cut_time.append(time[0])
@@ -80,7 +80,7 @@ def cutting_subject_name(name):  # 과목이름 중 영문 명 삭제
     return new_name
 
 
-
+#department1 : 개설학과 2: 주관학과
 def insert(index, grade, department1, department2, completion, field1, id, name, classroom, credit, limit_student,
            sugang_student, close_student, sugang_division, professor, time, note, major, field2):  # 데이터베이스에 정보를 추가
     sql = '''insert into subject 
@@ -91,90 +91,88 @@ def insert(index, grade, department1, department2, completion, field1, id, name,
                                          field2)
     curs.execute(sql)
 
-def search(search_word, grade, credit, completion):  # 검색어, 학년, 학점, 이수구분을 이용한 검색기능
+def search(search_word, time, grade, credit, completion):  # 검색어, 학년, 학점, 이수구분을 이용한 검색기능
     first = False
     sql = "select * from subject"
-    if search_word != "0":  # 검색어가 전체가 아닐 때
-        sql += " where sname like \"%" + search_word + "%\""
+    if search_word != "-1":  # 검색어가 전체가 아닐 때
+        sql += " where name like \"%" + search_word + "%\""
         first = True
-    if grade != "0":
+    if grade != "-1":
         if not first:
-            sql += " where sgrade = \"" + grade + "\""
+            sql += " where grade = \"" + grade + "\""
             first = True
         else:
-            sql += " and sgrade = \"" + grade + "\""
-    if credit != "0":
+            sql += " and grade = \"" + grade + "\""
+    if credit != "-1":
         if not first:
-            sql += " where scredit = \"" + credit + "\""
+            sql += " where credit = \"" + credit + "\""
             first = True
         else:
-            sql += " and scredit = \"" + credit + "\""
-    if completion != "0":
+            sql += " and credit = \"" + credit + "\""
+    if completion != "-1":
         if not first:
-            sql += " where scompletion = \"" + completion + "\""
+            sql += " where completion = \"" + completion + "\""
             first = True
         else:
-            sql += " and scompletion = \"" + completion + "\""
+            sql += " and completion = \"" + completion + "\""
     sql += ";"
-    print(sql)
+    #print(sql)
     curs.execute(sql)
     rows = curs.fetchall()
-    for cur_row in rows:
-        for i in cur_row:
-            print(i)
+    return rows
 
 # 테이블에 순서대로 num(1부터 끝까지, 기본키) 학년 개설학과 주관학과 이수구분 영역 학수번호 과목명 강의실 학점 제한인원 수강인원 폐강인원 수강구분 교수
 # 시간, 비고, 전공, 영역 순서대로 저장
 curs = conn.cursor()
-curs.execute("drop table if exists subject cascade")
-curs.execute(""" # 수업 테이블 생성
-create table subject(
-                num int,
-                grade varchar(5),
-                department1 varchar(30),
-                department2 varchar(30),
-                completion varchar(30),
-                field1 varchar(30),
-                id varchar(30),
-                name varchar(100),
-                classroom varchar(50),
-                credit varchar(10),
-                limit_student varchar(30),
-                sugang_student varchar(30),
-                close_student varchar(30),
-                sugang_division varchar(30),
-                professor varchar(30),
-                time varchar(50),
-                note varchar(100), 
-                major varchar(30),
-                field2 varchar(30),
-                primary key (num));""")
+# curs.execute("drop table if exists subject cascade")
+# curs.execute(""" # 수업 테이블 생성
+# create table subject(
+#                 num int,
+#                 grade varchar(5),
+#                 department1 varchar(30),
+#                 department2 varchar(30),
+#                 completion varchar(30),
+#                 field1 varchar(30),
+#                 id varchar(30),
+#                 name varchar(100),
+#                 classroom varchar(50),
+#                 credit varchar(10),
+#                 limit_student varchar(30),
+#                 sugang_student varchar(30),
+#                 close_student varchar(30),
+#                 sugang_division varchar(30),
+#                 professor varchar(30),
+#                 time varchar(50),
+#                 note varchar(100),
+#                 major varchar(30),
+#                 field2 varchar(30),
+#                 primary key (num));""")
 
 # 크롤링한 데이터 DB에 넣음
-data = test_read.class_list
-index = 0
-for i in data:
-    for j in range(len(i)):
-        i[j].replace("\n", "")
-    if len(i) == 16:  # 영역 , 강의식이 없는 경우(싸강)
-        insert(index, i[0], i[1], i[2], i[3], "None_field", i[4], "None_classroom", i[5], i[6], i[7], i[8], i[9], i[10],
-               i[11], i[12], i[13], i[14], i[15])
-    elif len(i) == 17:  # 영역이 없는 경우
-        insert(index, i[0], i[1], i[2], i[3], "None_field", i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], i[12],
-               i[13], i[14], i[15], i[16])
-    else:
-        insert(index, i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], i[12], i[13], i[14],
-               i[15], i[16], i[17])
-    index += 1
+# data = test_read.class_list
+# index = 0
+# for i in data:
+#     for j in range(len(i)):
+#         i[j].replace("\n", "")
+#     if len(i) == 16:  # 영역 , 강의식이 없는 경우(싸강)
+#         insert(index, i[0], i[1], i[2], i[3], "None_field", i[4], "None_classroom", i[5], i[6], i[7], i[8], i[9], i[10],
+#                i[11], i[12], i[13], i[14], i[15])
+#     elif len(i) == 17:  # 영역이 없는 경우
+#         insert(index, i[0], i[1], i[2], i[3], "None_field", i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], i[12],
+#                i[13], i[14], i[15], i[16])
+#     else:
+#         insert(index, i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10], i[11], i[12], i[13], i[14],
+#                i[15], i[16], i[17])
+#     index += 1
 
 # DB 테스트 출력
-sql = ("select * from subject")
-curs.execute(sql)
-rows = curs.fetchall()
-for cur_row in rows:
-    for i in cur_row:
-        print(i, end=",")
-    print()
+#sql = ("select * from subject")
+#curs.execute(sql)
+# search("소프트웨어", "-1", "-1", "-1", "-1")
+# rows = curs.fetchall()
+# for cur_row in rows:
+#     for i in cur_row:
+#         print(i, end=",")
 
 conn.commit()
 conn.close()
