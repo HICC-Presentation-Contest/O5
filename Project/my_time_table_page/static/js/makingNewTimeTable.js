387bb96b6de8b90e6c2a5a21eec894a2de238c8f
yooooonshine <hl3470@naver.com>
2023-06-26 18:07:42 +0900

    Fix: makingNewTimeTable.js 오류 수정

--- Project/my_time_table_page/static/js/makingNewTimeTable.js
+++ Project/my_time_table_page/static/js/makingNewTimeTable.js
@@ -18,8 +18,6 @@ function getCookie(name) {
 var csrftoken = getCookie('csrftoken');
 
 
-
-
 // 그룹의 번호를 재할당하는 함수
 function sync_group_numbers() {
     var groups = document.getElementsByClassName('original');
@@ -41,7 +39,7 @@ function add_group() {
     var field = document.getElementById('field');
     var groupNumber = sync_group_numbers() + 1;        //그룹의 수 1증가
 
-      if (groupNumber > 10) { // 최대 10개까지 추가 가능
+    if (groupNumber > 10) { // 최대 10개까지 추가 가능
         alert("그룹은 최대 10개까지 추가할 수 있습니다.");
         return;
     }
@@ -77,7 +75,7 @@ function add_item(parentNode) {
 function remove_group(group) {
     var remainingGroups = sync_group_numbers();
 
-     // 그룹의 수가 2개 이하인 경우 삭제 불가능함
+    // 그룹의 수가 2개 이하인 경우 삭제 불가능함
     if (remainingGroups <= 2) {
         alert("시간표를 만들기 위해 2개 이상의 그룹이 필요합니다.");
     } else {
@@ -93,155 +91,83 @@ function remove_group(group) {
 function remove_item(obj) {
     obj.parentNode.parentNode.removeChild(obj.parentNode);
 }
+
 // 시간표 데이터
 var timetable = [
-  { major: "대예이", time: "월23", year: 3, credit: 3 },
-  { major: "컴프", time: "화13", year: 2, credit: 2 },
-  { major: "대학물리", time: "월10", year: 4, credit: 4 },
-  { major: "영어", time: "화09", year: 3, credit: 1 },
+    {major: "대예이", time: "월23", year: 3, credit: 3},
+    {major: "컴프", time: "화13", year: 2, credit: 2},
+    {major: "대학물리", time: "월10", year: 4, credit: 4},
+    {major: "영어", time: "화09", year: 3, credit: 1},
 ];
 
 function sortTimetable(sortKey) {
-  timetable.sort(function(a, b) {
-    if (sortKey === "time") {
-      // 시간에 대한 정렬
-      var dayA = a.time.substr(0, 2);
-      var dayB = b.time.substr(0, 2);
-      var hourA = parseInt(a.time.substr(2));
-      var hourB = parseInt(b.time.substr(2));
+    timetable.sort(function (a, b) {
+        if (sortKey === "time") {
+            // 시간에 대한 정렬
+            var dayA = a.time.substr(0, 2);
+            var dayB = b.time.substr(0, 2);
+            var hourA = parseInt(a.time.substr(2));
+            var hourB = parseInt(b.time.substr(2));
 
-      if (dayA !== dayB) {
-        // 요일이 다른 경우
-        var days = ["월", "화", "수", "목", "금"];
-        return days.indexOf(dayA) - days.indexOf(dayB);
-      } else {
-        // 요일이 같은 경우
-        return hourA - hourB;
-      }
-    } else {
-      // 다른 키에 대한 정렬
-      return a[sortKey] > b[sortKey] ? 1 : -1;
-    }
-  });
+            if (dayA !== dayB) {
+                // 요일이 다른 경우
+                var days = ["월", "화", "수", "목", "금"];
+                return days.indexOf(dayA) - days.indexOf(dayB);
+            } else {
+                // 요일이 같은 경우
+                return hourA - hourB;
+            }
+        } else {
+            // 다른 키에 대한 정렬
+            return a[sortKey] > b[sortKey] ? 1 : -1;
+        }
+    });
 
-  displayTimetable();
+    displayTimetable();
 }
 
 function displayTimetable() {
-  var table = document.getElementById("timetable");
+    var table = document.getElementById("timetable");
 
-  // 기존 테이블 내용 삭제
-  while (table.firstChild) {
-    table.removeChild(table.firstChild);
-  }
-
-  // 정렬된 시간표 출력
-  timetable.forEach(function(course) {
-    var row = document.createElement("tr");
-
-    // 과목
-    var majorCell = document.createElement("td");
-    majorCell.textContent = course.major;
-    row.appendChild(majorCell);
-
-    // 시간
-    var timeCell = document.createElement("td");
-    timeCell.textContent = course.time;
-    row.appendChild(timeCell);
-
-    // 학년
-    var yearCell = document.createElement("td");
-    yearCell.textContent = course.year;
-    row.appendChild(yearCell);
-
-    // 학점
-    var creditCell = document.createElement("td");
-    creditCell.textContent = course.credit;
-    row.appendChild(creditCell);
-
-    table.appendChild(row);
-  });
-}
-
-// resultBox에 요소 넣기
-var textList = [['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-1', '미술의이해\n(COMPREHENSION OF ART)', 'C506', '3', '10/20/30/40/50', '55', '20', '비공학', '전영백', '화789', '미술대 수강불가/강의요원 여서영 ', '공통교양\n(서울)', '예술과디자인'],
-    ['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-2', '미술의이해\n(COMPREHENSION OF ART)', 'C807', '3', '10/20/30/40/50', '47', '20', '공학', '손수연', '화789', '미술대 수강불가 ', '공통교양\n(서울)', '예술과디자인']]
-function pushClassData(textData) {
-  var a = document.createElement("div");
-  a.innerHTML = '<div class="result">' +
-    '<div class="subject">' + textData[6] + '</div>' +
-    '<div class="nameTime">' + textData[13] + ' ' + textData[14] + '</div> ' +
-    '<div class="detail">' + textData[0] + '학년 ' + textData[3] + ' ' + textData[8] + '학점 ' + textData[5] + '</div>' +
-    '</div>';
-  document.getElementById("resultBox").appendChild(a);
-}
-
-function displayTextList() {
-  for (var i = 0; i < textList.length; i++) {
-    pushClassData(textList[i]);
-  }
-}
-
-function sendingData(inp) { //inp는 input객체
-
-    if (inp == document.getElementById('autoInput')) {
-        boolDepartureCheck = false;
-    } else {
-        boolDestinationCheck = false;
+    // 기존 테이블 내용 삭제
+    while (table.firstChild) {
+        table.removeChild(table.firstChild);
     }
-    $.ajax({
-        url: 'recommend',
-        type: 'POST',
-        data: {
-            'input_val': inp.value.toUpperCase(), //대문자 변환해서 소문자도 검색가능
-            'csrfmiddlewaretoken': csrftoken,
-        },
-        datatype: 'json',
-        success: function (data) {
-            receivedList = data['recommendations'];
-            autocomplete.setAutocomplete(inp, receivedList); //autocomplete함수를 input객체를 받아 실행
-            autocomplete.inputEvent(nowKeyboardCode);
-            autocomplete.keydownEvent(nowKeyboardCode);
-            console.log(receivedList);
-        }
+
+    // 정렬된 시간표 출력
+    timetable.forEach(function (course) {
+        var row = document.createElement("tr");
+
+        // 과목
+        var majorCell = document.createElement("td");
+        majorCell.textContent = course.major;
+        row.appendChild(majorCell);
+
+        // 시간
+        var timeCell = document.createElement("td");
+        timeCell.textContent = course.time;
+        row.appendChild(timeCell);
+
+        // 학년
+        var yearCell = document.createElement("td");
+        yearCell.textContent = course.year;
+        row.appendChild(yearCell);
+
+        // 학점
+        var creditCell = document.createElement("td");
+        creditCell.textContent = course.credit;
+        row.appendChild(creditCell);
+
+        table.appendChild(row);
     });
 }
 
+// 검색창 입력 문자 확인
+let nowKeyboardCode = 0;
+$('#autoInput').bind('keydown', function (e) {
+    nowKeyboardCode = e.keyCode;
 
-function submitCheck(event) {
-    //submit될 때 페이지 리로드 방지
-    //event.preventDefault();
-    var timeTableQuery = document.getElementById("autoInput").value;
-
-    //출발지 도착지형식이 참이면 백으로 출발지 도착지 보내기
-
-        //결과경로창 보이게끔
-
-    $.ajax({
-        url: 'query_submit',
-        type: 'POST',
-        data: {
-            'time_table_query': timeTableQuery,
-            'csrfmiddlewaretoken': csrftoken,
-        },
-        datatype: 'json',
-        beforeSend: function (request) {
-            // Performed before calling Ajax
-            $("#mySpinner").show();
-
-        },
-        success: function (data) {
-
-            $("#mySpinner").hide();
-            // textList = data;
-            pushClassData(textList)
-        },
-
-    });
-
-}
-
-
+})
 
 //##자동완성
 // autocomplete 부분을 생성
@@ -350,14 +276,14 @@ let autocomplete = (function () {
             // enter
             if (_inp == document.getElementById('autoInput')) {
                 for (let list in $('#autoInput1autocomplete-list div')) {
-                    if (list.html == _inp.innerHTML){
+                    if (list.html == _inp.innerHTML) {
                         boolDepartureCheck = true;
                     }
                 }
 
             } else {
                 for (let list in $('#autoInputautocomplete-list div')) {
-                    if (list.html == _inp.innerHTML){
+                    if (list.html == _inp.innerHTML) {
                         boolDestinationCheck = true;
                     }
                 }
@@ -439,3 +365,87 @@ let autocomplete = (function () {
     }
 
 })();
+
+let receivedList = [];
+
+function suggestedSearchWord(inp) { //inp는 input객체
+    $.ajax({
+        url: 'suggestedSearchWord',
+        type: 'POST',
+        data: {
+            'search_word': inp.value.toUpperCase(), //대문자 변환해서 소문자도 검색가능
+            'csrfmiddlewaretoken': csrftoken,
+        },
+        datatype: 'json',
+        success: function (data) {
+            receivedList = data['suggested_search_word_list'];
+            autocomplete.setAutocomplete(inp, receivedList); //autocomplete함수를 input객체를 받아 실행
+            autocomplete.inputEvent(nowKeyboardCode);
+            autocomplete.keydownEvent(nowKeyboardCode);
+            console.log(receivedList);
+        }
+    });
+}
+
+
+// resultBox에 요소 넣기
+var textList = [['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-1', '미술의이해\n(COMPREHENSION OF ART)', 'C506', '3', '10/20/30/40/50', '55', '20', '비공학', '전영백', '화789', '미술대 수강불가/강의요원 여서영 ', '공통교양\n(서울)', '예술과디자인'],
+    ['0', '교양과(서울)', '예술학과', '교선', '예술과디자인', '002056-2', '미술의이해\n(COMPREHENSION OF ART)', 'C807', '3', '10/20/30/40/50', '47', '20', '공학', '손수연', '화789', '미술대 수강불가 ', '공통교양\n(서울)', '예술과디자인']]
+
+function pushClassData(textData) {
+    var a = document.createElement("div");
+    a.innerHTML = '<div class="result">' +
+        '<div class="subject">' + textData[6] + '</div>' +
+        '<div class="nameTime">' + textData[13] + ' ' + textData[14] + '</div> ' +
+        '<div class="detail">' + textData[0] + '학년 ' + textData[3] + ' ' + textData[8] + '학점 ' + textData[5] + '</div>' +
+        '</div>';
+    document.getElementById("resultBox").appendChild(a);
+}
+
+function displayTextList() {
+    for (var i = 0; i < textList.length; i++) {
+        pushClassData(textList[i]);
+    }
+}
+
+
+// 사용자가 검색버튼을 눌렀을떄
+function searchWordSubmit() {
+    //submit될 때 페이지 리로드 방지
+    //event.preventDefault();
+    var searchWord = document.getElementById("autoInput").value;
+
+    //출발지 도착지형식이 참이면 백으로 출발지 도착지 보내기
+
+    //결과경로창 보이게끔
+
+    $.ajax({
+        url: 'searchWordSubmit',
+        type: 'POST',
+        // data부분은 딕셔너리로 넘겨준다
+        data: {
+            'search_word': searchWord,
+            'csrfmiddlewaretoken': csrftoken,
+        },
+        datatype: 'json',
+        beforeSend: function (request) {
+            // Performed before calling Ajax
+            $("#mySpinner").show();
+
+        },
+        success: function (data) {
+
+            $("#mySpinner").hide();
+            // textList = data;
+            displayTextList()
+        },
+
+    });
+
+}
+
+
+function groupCheck() {
+    document.getElementById("searchButton").disabled = true;
+}
+
