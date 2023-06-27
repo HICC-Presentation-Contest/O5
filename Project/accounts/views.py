ef44b097e30d9303e4f03decd53e4934134900fe
yooooonshine <hl3470@naver.com>
2023-06-27 16:47:05 +0900

    Feat: 회원가입 페이지

--- Project/accounts/views.py
+++ Project/accounts/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+
+
 # Create your views here.
 def login_view(request):
     if request.method == "POST":
         username = request.POST["id"]
         password = request.POST["password"]
-        user = authenticate(username=username, password=password) # 인증확인 후 틀리면 None배출
+        user = authenticate(username=username, password=password)  # 인증확인 후 틀리면 None배출
         if user is not None:
             print("인증성공")
             login(request, user)
         else:
             print("인증실패")
-    return render(request,"accounts/login.html")
+    return render(request, "accounts/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("users:login")
+
+def signup_view(request):
+    return render(request, "accounts/signup.html")
