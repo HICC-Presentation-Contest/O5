8b0dae786ae146aecb92250f9e379074dc7c5c9b
yooooonshine <hl3470@naver.com>
2023-06-27 15:58:55 +0900

    Feat: 로그인 입력기능 구현완료

--- Project/accounts/views.py
+++ Project/accounts/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def login_view(request):
     if request.method == "POST":
         username = request.POST["id"]
         password = request.POST["password"]
-        print(username,password)
+        user = authenticate(username=username, password=password) # 인증확인 후 틀리면 None배출
+        if user is not None:
+            print("인증성공")
+            login(request, user)
+        else:
+            print("인증실패")
     return render(request,"accounts/login.html")
