9b97689581e2854ca114d2c7901238891670c0f5
hyojin <holic484@gmail.com>
2023-07-05 11:12:21 +0900

    FIX: 아이디 이메일 중복체크

--- Project/accounts/static/js/signup.js
+++ Project/accounts/static/js/signup.js
@@ -40,3 +40,51 @@ $(function(){
         }
     });
 });
+//이메일 중복체크
+var emoverChk = false;
+	$("#emoverlay").click(function(){
+		var email = $("#email").val();
+			$.ajax({
+				type:'post'
+				,url:'emoverlay'
+				,data:{'email' : email }
+				,dataType:'JSON'
+				,success:function(obj){
+					console.log(obj);
+					if(obj.use2 != 1){
+						alert('사용할 수 있는 이메일 입니다.');
+						emoverChk= true;
+					}else{
+						alert('이미 사용중인 이메일 입니다.');
+
+					}
+				}
+				,error:function(e){
+					console.log(e);
+				}
+			});
+		});
+//아이디 중복체크
+ var overChk = false;
+		$("#overlay").click(function(){
+			var id = $("#username").val();
+				$.ajax({
+					type:'post'
+					,url:'overlay'
+					,data:{'id' : id }
+					,dataType:'JSON'
+					,success:function(obj){
+						console.log(obj);
+						if(obj.use != 1){
+							alert('사용할 수 있는 아이디 입니다.');
+							overChk= true;
+						}else{
+							alert('이미 사용중인 아이디 입니다.');
+
+						}
+					}
+					,error:function(e){
+						console.log(e);
+					}
+				});
+			});
